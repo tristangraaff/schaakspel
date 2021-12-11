d553208939b840fadd6844ddf1909dc74e0ff37a
tristangraaff <tristangraaff@yahoo.com>
2021-12-11 13:40:46 +0100

    added params to class constructor

--- pieces.js
+++ pieces.js
@@ -1,6 +1,10 @@
 class Piece {
-  constructor() {
-
+  constructor(position, name, color, rank, img) {
+    this.position = position;
+    this.name = name;
+    this.color = color;
+    this.rank = rank;
+    this.img = document.querySelector(this.img);
   }
 }
 
